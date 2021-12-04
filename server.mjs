81319c799c7a76e90019189abc4a430e6112b6fc
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2021-12-04 18:29:56 +0100

    test

--- server.mjs
+++ server.mjs
@@ -1,76 +1,91 @@
-import geckos, { iceServers } from '@geckos.io/server'
-import { readFile } from 'fs/promises'
-import { createServer } from 'http'
+import geckos, { iceServers } from "@geckos.io/server";
+import { readFile } from "fs/promises";
+import fs, { readdir } from "fs";
+import { createServer } from "http";
 
-const indexHtml = await readFile('index.html', { encoding: 'utf-8' })
-const geckosJs = await readFile('frontend/geckos.io-client.2.1.2.min.js', {
-  encoding: 'utf-8',
-})
+const indexHtml = await readFile("index.html", { encoding: "utf-8" });
+const geckosJs = await readFile("frontend/geckos.io-client.2.1.2.min.js", {
+  encoding: "utf-8",
+});
+const connect = await readFile("frontend/connect.js", {
+  encoding: "utf-8",
+});
+const p5 = await readFile("frontend/p5.min.js", {
+  encoding: "utf-8",
+});
+const sketch = await readFile("frontend/sketch.js", {
+  encoding: "utf-8",
+});
+const state = await readFile("frontend/state.js", {
+  encoding: "utf-8",
+});
 
-/*
 const requestListener = async (req, res) => {
-  if (req.url === '/')
-    return res.writeHead(200, { 'Content-Type': 'text/html' }).end(indexHtml)
+  if (req.url === "/")
+    return res.writeHead(200, { "Content-Type": "text/html" }).end(indexHtml);
 
-  if (req.url === '/frontend/geckos.io-client.2.1.2.min.js')
+  if (req.url === "/frontend/geckos.io-client.2.1.2.min.js")
     return res
-      .writeHead(200, { 'Content-Type': 'application/javascript' })
-      .end(geckosJs)
+      .writeHead(200, { "Content-Type": "application/javascript" })
+      .end(geckosJs);
 
-  return res.writeHead(404).end()
-}
+  if (req.url === "/frontend/connect.js")
+    return res
+      .writeHead(200, { "Content-Type": "application/javascript" })
+      .end(connect);
 
-const server = createServer(requestListener)
-*/
+  if (req.url === "/frontend/p5.min.js")
+    return res
+      .writeHead(200, { "Content-Type": "application/javascript" })
+      .end(p5);
+
+  if (req.url === "/frontend/sketch.js")
+    return res
+      .writeHead(200, { "Content-Type": "application/javascript" })
+      .end(sketch);
+
+  if (req.url === "/frontend/state.js")
+    return res
+      .writeHead(200, { "Content-Type": "application/javascript" })
+      .end(state);
+
+  return res.writeHead(404).end();
+};
+
+const server = createServer(requestListener);
 
 const io = geckos({
-  iceServers: process.env.NODE_ENV === 'production' ? iceServers : [],
-})
+  iceServers: process.env.NODE_ENV === "production" ? iceServers : [],
+});
 
-io.addServer(server)
+io.addServer(server);
 
 server.listen(3000, () => {
-  console.log('server running on http://127.0.0.1:3000')
-})
+  console.log("server running on http://127.0.0.1:3000");
+});
 
-/*
 io.onConnection((channel) => {
+  console.log(`${channel.id} connected`);
+  let player = {
+    id: channel.id,
+    x: 20,
+    y: 20,
+  };
+  players.push(player);
+  channel.emit("players", players);
+
   channel.onDisconnect(() => {
-    console.log(`${channel.id} got disconnected`)
-  })
-
-  channel.emit('chat message', `Welcome to the chat ${channel.id}!`)
-
-  channel.on('chat message', (data) => {
-    channel.room.emit('chat message', data)
-  })
-})
-*/
-
-
-io.onConnection((channel) => {
-    console.log(`${channel.id} connected`)
-    let player = {
-      id: channel.id,
-      x: 20,
-      y: 20
-    }
-    players.push(player);
-    channel.emit("players", players);
-  
-  
-    channel.onDisconnect(() => {
-      console.log(`${channel.id} got disconnected`);
-      players = players.filter(p => p.id != channel.id);
-    });
-  
-    channel.emit("chat message", `Welcome to the chat ${channel.id}!`);
-  
-    channel.on("chat message", (data) => {
-      channel.room.emit("chat message", data);
-    });
-  
-    channel.on("client:playerMoved", (data) => {
-      channel.room.emit("server:playerMoved", data);
-    });
+    console.log(`${channel.id} got disconnected`);
+    players = players.filter((p) => p.id != channel.id);
   });
+
+  channel.emit("chat message", `Welcome to the chat ${channel.id}!`);
+
+  channel.on("chat message", (data) => {
+    channel.room.emit("chat message", data);
+  });
+
+  channel.on("client:playerMoved", (data) => {
+    channel.room.emit("server:playerMoved", data);
+  });
+});
